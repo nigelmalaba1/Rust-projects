b49c0bcabb1a4fa8ceea7ac8d2d8673a19cc0c2e
EC2 Default User <ec2-user@ip-172-31-39-38.ec2.internal>
2023-03-21 04:38:08 +0000

    message

---
+++ Rust Palindrome Checker/palindrome/target/debug/deps/palindrome-5deb028853cecf16.d
@@ -0,0 +1,5 @@
+/home/ec2-user/environment/Rust-projects/Rust Palindrome Checker/palindrome/target/debug/deps/palindrome-5deb028853cecf16: src/main.rs
+
+/home/ec2-user/environment/Rust-projects/Rust Palindrome Checker/palindrome/target/debug/deps/palindrome-5deb028853cecf16.d: src/main.rs
+
+src/main.rs:
---
+++ Rust Palindrome Checker/palindrome/target/debug/palindrome.d
@@ -0,0 +1,1 @@
+/home/ec2-user/environment/Rust-projects/Rust\ Palindrome\ Checker/palindrome/target/debug/palindrome: /home/ec2-user/environment/Rust-projects/Rust\ Palindrome\ Checker/palindrome/src/main.rs
